19e87cd2f2d9307863fa9dd6d6742ec1f0f3b0a0
Keereslame <philippine.favre@hotmail.fr>
2025-03-16 15:58:06 +0100

    start testing readline and add libft to parse

---
+++ src/parse/include/parse.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfavre <alfavre@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 14:11:44 by alfavre           #+#    #+#             */
+/*   Updated: 2025/03/16 14:33:11 by alfavre          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_H
+# define PARSE_H
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include "../libft/include/libft.h"
+#endif
