cb73cc36edfdfb1a8ec031a5d8cb8821379a4514
keereslame <alexisPhilfavre@icloud.com>
2025-03-17 15:52:37 +0100

    add tokens struct

--- src/parse/include/parse.h
+++ src/parse/include/parse.h
@@ -6,7 +6,7 @@
 /*   By: alfavre <alfavre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 14:11:44 by alfavre           #+#    #+#             */
-/*   Updated: 2025/03/16 14:33:11 by alfavre          ###   ########.fr       */
+/*   Updated: 2025/03/17 15:49:43 by alfavre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,28 @@
 # include <readline/history.h>
 # include <stdlib.h>
 # include "../libft/include/libft.h"
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,					// Simple word, command, arguments
+	TOKEN_PIPE,					// |
+	TOKEN_REDIR_IN,				// <
+	TOKEN_REDIR_OUT,			// >
+	TOKEN_APPEND,				// >>
+	TOKEN_HEREDOC,				// <<
+	TOKEN_AND,					// &&
+	TOKEN_OR,					// ||
+	TOKEN_SEMICOLON,			// ;
+	TOKEN_OPEN_PARENTHESES,		// (
+	TOKEN_CLOSE_PARENTHESES,	// )
+	TOKEN_EOF					// End of line
+}	t_token_type;
+
+typedef struct	s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
 #endif
